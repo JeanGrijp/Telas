3c7ce1803b60a9baa5c0665db303f14ae822a448
Jean Grijp <grijpjean@gmail.com>
2021-02-09 18:52:58 -0300

    .

--- tela 01/frontend/src/routes.js
+++ tela 01/frontend/src/routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
-import home from './pages/home';
-import local from './pages/local';
+import home from './pages/home/';
+import local from './pages/local/';
 import profile from './pages/profile';
 import settingss from './pages/settingss';
 import search from './pages/search';
@@ -11,7 +11,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={home}/>
+        <Route path="/home" exact component={home}/>
         <Route path="/search" exact component={search}/>
         <Route path="/local" exact component={local}/>
         <Route path="/settingss" exact component={settingss}/>
