0a5b65f9f8ad1ac425fd0a5c11a4b775f05fc7c5
Jean Grijp <grijpjean@gmail.com>
2021-02-12 00:09:36 -0300

    .

--- Tela 02/main.js
+++ Tela 02/main.js
@@ -1,50 +1,45 @@
-let botao = document.querySelector('div.btn')
-if (botao.innerHTML === '<p>Sign up</p>') {
-  botao.addEventListener('click', () => {
+let botao = document.querySelector('div.btn > p')
+ 
+botao.addEventListener('click', () => {
+  if (botao.innerHTML === 'Sign up') {
     changeToSignUp()
-  })
-}
-if (botao.innerHTML === '<p>Sign in</p>') {
-  botao.addEventListener('click', () => {
-    changeToSignIn()
-  })
-}
+  }
+  else if (botao.innerHTML === 'Sign in') {
+      changeToSignIn()
+  }
+})
 
-async function changeToSignUp() {
-  // muda o valor do botão 
-   document.querySelector('div.btn > p').innerHTML = 'Sign in'
 
-  // muda o valor do tipo da página 
+const changeToSignUp = () => {
+  document.querySelector('div.container > img').setAttribute('src', './imagens/undraw_Code_thinking_re_gka2.svg')
+  document.querySelector('h2.tittle').innerHTML = 'Sign up'
+  document.querySelector('div.btn > p').innerHTML = 'Sign in'
   document.querySelector('.text > h1').innerHTML = "Have a Profile?" 
-
-  // remove o atributo class da primeira div após a div.container 
-  document.querySelector('div.container > div').removeAttribute('class')
-
-  // remove o atributo class da tag img filha da div.container
-  await console.log(  document.querySelectorAll('div.container > div'))
-  document.querySelectorAll('div.container > div')[1].removeAttribute('class')
-
-
-  document.querySelector('div.container > img').removeAttribute('class')
-
-
-  document.querySelector('div.container > div').setAttribute('class', 'text1')
-  document.querySelectorAll('div.container > div')[1].setAttribute('class', 'forms-container1')
-  document.querySelector('div.container > img').setAttribute('class', 'first-imagem-container1')
+  document.querySelector('div.container > div').classList.replace('text', 'text1')
+  document.querySelectorAll('div.container > div')[1].classList.replace('forms-container', 'forms-container1')
+  document.querySelector('div.container > img').classList.replace('first-imagem-container', 'first-imagem-container1')
+  document.querySelector('div.signin-signup').classList.replace('signin-signup', 'signin')
 
 
 }
 
 const changeToSignIn = () => {
-  document.querySelectorAll('div.container > div')[1].querySelectorAll('div > div').removeAttribute('class')
-
-
+  document.querySelector('div.container > img').setAttribute('src', './imagens/undraw_Data_trends_re_2cdy.svg')
+  document.querySelector('h2.tittle').innerHTML = 'Sign in'
+  // muda o valor do botão 
   document.querySelector('div.btn > p').innerHTML = 'Sign up'
-  document.querySelector('div.container > div').removeAttribute('class')
-  document.querySelectorAll('div.container > div')[1].removeAttribute('class')
-  document.querySelector('div.container > img').removeAttribute('class')
 
-  document.querySelector('div.container > div').setAttribute('class', 'text')
-  document.querySelectorAll('div.container > div')[1].setAttribute('class', 'forms-container')
-  document.querySelector('div.container > img').setAttribute('class', 'first-imagem-container')
+  // muda o valor do tipo da página 
+  document.querySelector('.text1 > h1').innerHTML = "New Here?" 
+
+  // altera o valor class da primeira div após a div.container 
+  document.querySelector('div.container > div').classList.replace('text1', 'text')
+
+  // remove o atributo class da segunda div filha do div.container
+  document.querySelectorAll('div.container > div')[1].classList.replace('forms-container1', 'forms-container')
+
+  // remove a classe da tag img 
+  document.querySelector('div.container > img').classList.replace('first-imagem-container1', 'first-imagem-container')
+
+  document.querySelector('div.signin').classList.replace('signin', 'signin-signup')
 }
